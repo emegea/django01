bee4be134783e981022de0c63277c6cf1e54a119
emegea <martosproject@gmail.com>
2024-07-05 11:01:03 -0300

    agregué un archivito para crear el superusuario con los datos de render.com

---
+++ proyecto01/crear_superusuario.py
@@ -0,0 +1,20 @@
+import os
+import django
+from django.contrib.auth import get_user_model
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tu_proyecto.settings')
+django.setup()
+
+User = get_user_model()
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+
+if username and email and password:
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username=username, email=email, password=password)
+        print('Superusuario creado correctamente.')
+    else:
+        print('El superusuario ya existe.')
+else:
+    print('Faltan variables de entorno para la creación de superusuario.')
