5115c7680fcbf144c66b37cf5055e198e97d839e
emegea <martosproject@gmail.com>
2024-07-05 10:17:10 -0300

    agregando creación de super usuario al build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,12 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py createsuperuser
-webmaster
-martosproject@gmail.com
-1234@admin
+
 python manage.py migrate
+# Variables de entorno para crear un superusuario
+export DJANGO_SUPERUSER_USERNAME=webmaster
+export DJANGO_SUPERUSER_EMAIL=martosproject@gmail.com
+export DJANGO_SUPERUSER_PASSWORD=1234@admin
+
+# Crear el superusuario no interactivamente
+python manage.py createsuperuser --no-input
--- proyecto01/settings.py
+++ proyecto01/settings.py
@@ -151,6 +151,17 @@ DATABASES = {
 #    }
 }
 
+# Configuración para el superusuario
+if 'DJANGO_SUPERUSER_USERNAME' in os.environ:
+    from django.contrib.auth import get_user_model
+
+    User = get_user_model()
+    if not User.objects.filter(username=os.environ['DJANGO_SUPERUSER_USERNAME']).exists():
+        User.objects.create_superuser(
+            username=os.environ['DJANGO_SUPERUSER_USERNAME'],
+            email=os.environ['DJANGO_SUPERUSER_EMAIL'],
+            password=os.environ['DJANGO_SUPERUSER_PASSWORD']
+        )
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
