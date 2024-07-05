55ae6707c0d1f440c338cca98045e550187325ad
emegea <martosproject@gmail.com>
2024-07-05 11:04:36 -0300

    agregué un archivito para crear el superusuario con los datos de render.com

--- proyecto01/crear_superusuario.py
+++ proyecto01/crear_superusuario.py
@@ -2,7 +2,7 @@ import os
 import django
 from django.contrib.auth import get_user_model
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tu_proyecto.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proyecto01.settings')
 django.setup()
 
 User = get_user_model()
