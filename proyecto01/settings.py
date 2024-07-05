4f7dce4873f34cea2a10a0bf615fac1bb4c6351f
emegea <martosproject@gmail.com>
2024-07-05 09:02:16 -0300

    deployando

---
+++ proyecto01/settings.py
@@ -0,0 +1,221 @@
+from pathlib import Path
+import os, dj_database_url
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = 'django-insecure-4g=@zx_w3vfb#a0id68tca0y)sbgqtp&l0leg_oler^p8622f#'
+# Secret Key de Render.com
+SECRET_KEY = os.environ.get('SECRET_KEY', default='mi secret key')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = True
+# Si no estoy en render, entonces DEBUG = True
+DEBUG = 'RENDER' not in os.environ
+
+ALLOWED_HOSTS = []
+# Confgurando el host de Render que nos provee render.com
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
+# Apps
+INSTALLED_APPS = [
+    'jazzmin',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'web'
+]
+
+# Opciones de Jazzmin (para customizar /admin)
+JAZZMIN_SETTINGS = {
+    "site_title": "Canchas Cancheras Admin",
+    "site_header": "Canchas Cancheras",
+    "site_brand": "Canchas Cancheras",
+    "site_logo": "web/img/iconoBlanco.svg",
+    "site_logo_classes": "img-circle logoAdmin",
+    "site_icon": "web/img/iconoColor.svg",
+    # Login
+    "login_logo": "web/img/logo.svg",
+    "login_logo_dark": "web/img/logo.svg",
+    "welcome_sign": "Bienvenido a Canchas Cancheras",
+    # Footer
+    "copyright": "Emegea",
+    # Usuario
+    "user_avatar": "web/img/icono.svg",
+    "show_sidebar": True,
+    "navigation_expanded": True,
+    "order_with_respect_to": ["auth", "web"],
+    # Custom CSS y JS
+    # "custom_css": "web/css/customAdmin.css",
+    # "custom_js": "web/js/customAdmin.js",
+    "icons": {
+        "auth": "fas fa-users-cog",
+        "auth.user": "fas fa-user",
+        "auth.Group": "fas fa-users",
+        "web.Cancha": "fas fa-regular fa-futbol",
+        "web.Cliente": "fas fa-users",
+        "web.Venta": "fas fa-money-bill",
+        "web.MensajeContacto": "fas fa-envelope",
+    },
+    "custom_links": {
+        "web": [{
+            "name": "Visitar el sitio",
+            "url": "index",
+            "icon": "fas fa-external-link-alt",
+            "permissions": ["auth.view_user"]
+        }]
+    },
+    # Render out the change view as a single form, or in tabs, current options are
+    # - single
+    # - horizontal_tabs (default)
+    # - vertical_tabs
+    # - collapsible
+    # - carousel
+    "changeform_format": "horizontal_tabs",
+    "changeform_format_overrides": {
+        "auth.user": "horizontal_tabs",
+        "auth.group": "horizontal_tabs",
+        "web.cancha": "horizontal_tabs",
+        "web.cleinte": "horizontal_tabs",
+        "web.ventas": "horizontal_tabs"
+        },
+    # Mostrar botón para acceder a la barra de personalización UI
+    "show_ui_builder": True,
+}
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+]
+
+ROOT_URLCONF = 'proyecto01.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'web/templates/web/')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'proyecto01.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
+DATABASES = {
+    # Trabajando con Render
+    'default': dj_database_url.config(
+        default = 'postgresql://postgres:postgres@localhost/postgres'
+    )
+    
+#    'default': {
+# ------------------------------------------------- #
+#        Lo que viene por defecto
+#
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#
+# ------------------------------------------------- #
+#        Trabajando en local con postgresql
+#
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'canchas',
+#        'USER': 'postgres',
+#        'PASSWORD': '1234',
+#        'HOST': '127.0.0.1',
+#        'PORT': '5432',
+#    }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/5.0/topics/i18n/
+
+LANGUAGE_CODE = 'es'
+
+TIME_ZONE = 'America/Argentina/Buenos_Aires'
+
+USE_I18N = True
+
+USE_TZ = True
+
+# Configurar los idiomas disponibles
+LANGUAGES = [
+    ('es', 'Español'),
+]
+# Ruta a los archivos de traducción
+LOCALE_PATHS = [
+    BASE_DIR / 'locale',
+]
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.0/howto/static-files/
+
+STATIC_URL = 'static/'
+if not DEBUG:
+    # Le digo a Django que copie los archivos de 'static'
+    # en 'staticfiles' (esto es específico de Render).
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticdiles')
+    # Habilito el almacenamiento de backend WhiteNoise que comprime
+    # archivos estáticos para reducir el uso del disco y cambia el
+    # nombre de los archivos con nombres únicos para cada versión,
+    # para admitir el almacenamiento en caché a largo plazo
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Redireccionando URL por defecto
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+# Configuraciones de email
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# Acá estamos usando la consola para ver los mails.
---
+++ web/views.py
@@ -0,0 +1,274 @@
+from django.http import HttpResponse
+import datetime
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout
+from django.contrib.auth.models import Group
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator 
+from django.views.generic import CreateView, View
+from django.urls import reverse_lazy
+from django.views import View
+from .models import *
+from .forms import *
+
+# Vista del Index
+def index(request):
+    # Obtengo las últimas 4 canchas publicadas
+    canchas = Cancha.objects.all().order_by('-id')[:4]
+
+    contexto = {
+        'nombre': 'Carlos',
+        'fecha_hora': datetime.datetime.now(),
+        'canchas': canchas
+    }
+    return render(request, 'index.html', {'contexto': canchas})
+
+# Vista de Quienes Somos
+def somos(request):
+    return render(request, "somos.html")
+
+# Vista de Canchas
+def canchas(request):
+    canchas = Cancha.objects.all()
+    return render(request, 'canchas.html', {'canchas': canchas})
+
+# Vista de Contacto
+def contacto(request):
+    contexto = {}
+    if request.method == 'GET':
+        contexto['formulario_contacto'] = formularioContacto()
+    else:
+        formulario = formularioContacto(request.POST)
+        contexto['formulario_contacto'] = formulario
+        if formulario.is_valid():
+            formulario.save()
+            messages.success(request, "El mensaje se envió correctamente")
+            return redirect('contacto')
+    return render(request, "contacto.html", contexto)
+
+# Vista de Sugerencia (excepciones)
+def enviar_sugerencia(request):
+    if request.method == 'POST':
+        form = SugerenciaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('gracias')
+    else:
+        form = SugerenciaForm()
+
+    return render(request, 'enviar_sugerencia.html', {'form': form})
+
+# Vista de Gracias
+def gracias(request):
+    contenido = "Mensaje de agradecimiento personalizado"
+    MensajeAgradecimiento.objects.create(contenido=contenido)
+    return render(request, 'gracias.html')
+
+
+### VISTAS DE COMPRAS
+
+
+#Vista de búsqueda parametrizada filtrando
+@login_required(login_url='/login/')
+def buscar(request):
+    if not request.user.is_authenticated:
+        return redirect('login')
+    
+    canchas = []
+
+    if request.method == "GET" and not request.GET:
+        formulario = CanchaForm()
+    else:
+        formulario = CanchaForm(request.GET)
+        if formulario.is_valid():
+            tipo_suelo = formulario.cleaned_data.get('tipo_suelo')
+            tipo_red = formulario.cleaned_data.get('tipo_red')
+            iluminacion = formulario.cleaned_data.get('iluminacion')
+            marcador = formulario.cleaned_data.get('marcador')
+            gradas = formulario.cleaned_data.get('gradas')
+
+            canchas = Cancha.objects.all()  
+            if tipo_suelo:
+                canchas = canchas.filter(tipo_suelo=tipo_suelo)
+            if tipo_red:
+                canchas = canchas.filter(tipo_red=tipo_red)
+            if iluminacion is not None:
+                canchas = canchas.filter(iluminacion=iluminacion)
+            if marcador is not None:
+                canchas = canchas.filter(marcador=marcador)
+            if gradas is not None:
+                canchas = canchas.filter(gradas=gradas)
+
+    return render(request, 'buscar.html', {
+        'formulario': formulario,
+        'canchas': canchas
+    })
+
+# Vista para manejar la compra de una cancha estándar
+def comprar_cancha(request, cancha_id):
+    cancha = get_object_or_404(Cancha, id=cancha_id)
+    
+    if request.method == 'POST':
+        cliente_form = ClienteForm(request.POST)
+        venta_form = VentaForm(request.POST)
+        
+        if cliente_form.is_valid() and venta_form.is_valid():
+            cliente = cliente_form.save()
+            venta = venta_form.save(commit=False)
+            venta.cliente = cliente
+            venta.save()
+            venta.canchas.add(cancha)
+            messages.success(request, "Compra realizada con éxito!")
+            return redirect('gracias')
+    else:
+        cliente_form = ClienteForm()
+        venta_form = VentaForm()
+    
+    return render(request, 'comprar_cancha.html', {
+        'cancha': cancha,
+        'cliente_form': cliente_form,
+        'venta_form': venta_form
+    })
+
+# Vista de Venta Personalizada
+def cancha_personalizada(request):
+    if request.method == 'POST':
+        cancha_form = CanchaForm(request.POST)
+        cliente_form = ClienteForm(request.POST)
+        venta_form = VentaForm(request.POST)
+        
+        if cancha_form.is_valid() and venta_form.is_valid() and cliente_form.is_valid():
+            cliente = cliente_form.save()
+            # cancha = cancha_form.save()
+            venta = venta_form.save(commit=False)
+            venta.cliente = cliente
+            venta.is_custom = True  # Marcar la venta como personalizada
+            venta.save()
+            venta.canchas.add()  # Agregar la cancha a la venta
+            messages.success(request, "¡Venta concretada!")
+        return redirect('/gracias')
+    else:
+        cancha_form = CanchaForm()
+        venta_form = VentaForm()
+        cliente_form = ClienteForm()
+
+    return render(request, 'cancha_personalizada.html', {
+        'cancha_form': cancha_form,
+        'venta_form': venta_form,
+        'cliente_form': cliente_form,
+    })
+
+
+### VISTAS AUTH
+
+
+# Redirigir usuarios autenticados (decorador custom)
+def redirigirAutenticados(view_func):
+    def _wrapped_view(request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('/')
+        return view_func(request, *args, **kwargs)
+    return _wrapped_view
+
+# Vista de Registro de Usuarios
+@redirigirAutenticados
+def registro(request):
+    if request.method == 'POST':
+        formulario_registro = formularioRegistro(request.POST)
+        if formulario_registro.is_valid():
+            user = formulario_registro.save()
+            # Asigna el nuevo usuario al grupo Clientes
+            group = Group.objects.get(name='Clientes')
+            user.groups.add(group)
+            login(request, user)
+            messages.success(request, "Usuario registrado correctamente.")
+            return redirect('index')
+
+    else:
+        formulario_registro = formularioRegistro()
+    return render(request, 'registro.html', {'formulario_registro': formulario_registro})
+
+# Vista de Login (No está funcionando, usamnos la de shango)
+@redirigirAutenticados
+def vistaLogin(request):
+    if request.user.is_authenticated:
+        return redirect('index')
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            messages.success(request, 'Inicio de sesión exitoso.')
+            # Redirige a la página admin después del login
+            return redirect('/admin/') 
+    else:
+        form = AuthenticationForm()
+    
+    return render(request, 'web/login.html', {'formulario_login': form})
+
+# Vista de Logout
+def vistaLogout(request):
+    #Llamo al logout de django
+    logout(request)
+    messages.success(request, "El usuario cerró su sesión correctamente.")
+    return redirect('index')
+
+### VISTAS DE ADMINISTRACIÓN (Vistas basadas en clases)
+
+# Vista de Admin Home
+def admin(request):
+    return redirect('/admin')
+
+# Vista para crear una Cancha
+@method_decorator(login_required(login_url='/login/'), name='dispatch')
+class CrearCancha(View):
+    def get(self, request):
+        form = CanchaForm()
+        return render(request, 'crear_cancha.html', {'form': form})
+
+    def post(self, request):
+        form = CanchaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Cancha creada correctamente")
+            return redirect('listar_canchas')
+        return render(request, 'crear_cancha.html', {'form': form})
+
+# Vista para listar todas las Canchas
+@method_decorator(login_required(login_url='/login/'), name='dispatch')
+class ListarCanchas(View):
+    def get(self, request):
+        canchas = Cancha.objects.all()
+        return render(request, 'listar_canchas.html', {'canchas': canchas})
+
+# Vista para actualizar una Cancha
+@method_decorator(login_required(login_url='/login/'), name='dispatch')
+class ActualizarCancha(View):
+    def get(self, request, id):
+        cancha = get_object_or_404(Cancha, id=id)
+        form = CanchaForm(instance=cancha)
+        return render(request, 'actualizar_cancha.html', {'form': form})
+
+    def post(self, request, id):
+        cancha = get_object_or_404(Cancha, id=id)
+        form = CanchaForm(request.POST, instance=cancha)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Cancha actualizada correctamente")
+            return redirect('listar_canchas')
+        return render(request, 'actualizar_cancha.html', {'form': form})
+
+# Vista para eliminar una Cancha
+@method_decorator(login_required(login_url='/login/'), name='dispatch')
+class EliminarCancha(View):
+    def get(self, request, id):
+        cancha = get_object_or_404(Cancha, id=id)
+        return render(request, 'eliminar_cancha.html', {'cancha': cancha})
+
+    def post(self, request, id):
+        cancha = get_object_or_404(Cancha, id=id)
+        cancha.delete()
+        messages.success(request, "Cancha eliminada correctamente")
+        return redirect('listar_canchas')
